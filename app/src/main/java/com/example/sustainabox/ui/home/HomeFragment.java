b4826edaa51e6cd51d49643b45fa64b7d0864758
wesselvanderwijst <w.p.r.v.d.wijst@student.tue.nl>
2024-03-02 21:47:19 +0100

    availableCredits now based on user credits, progressbar not working anymore

--- app/src/main/java/com/example/sustainabox/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sustainabox/ui/home/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.sustainabox.ui.home;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,14 +20,31 @@ import com.example.sustainabox.CaptureAct;
 import com.example.sustainabox.MainActivity;
 import com.example.sustainabox.User;
 import com.example.sustainabox.databinding.FragmentHomeBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
+import com.example.sustainabox.R;
+import com.example.sustainabox.User;
+import com.example.sustainabox.ui.LoginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private int totalCredits;
     private int availableCredits;
+    private DatabaseReference mDatabase;
+    private String userId;
+// ...
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -39,10 +57,10 @@ public class HomeFragment extends Fragment {
         final Button qrScannerButton = binding.openQrScannerButton;
         homeViewModel.getQRButtonText().observe(getViewLifecycleOwner(), qrScannerButton::setText);
 
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        mDatabase = FirebaseDatabase.getInstance().getReference("Users");
         // This needs to be changed later to get this info from database, but I can't figure out how to do that.
         totalCredits = 5;
-        availableCredits = 3;
-
         updateCreditDisplay();
 
         return root;
@@ -55,6 +73,7 @@ public class HomeFragment extends Fragment {
     }
 
     private void updateCreditDisplay() {
+        getUserCredits(mDatabase);
         TextView creditDisplay = binding.creditDisplayText;
         ProgressBar creditProgressBar = binding.creditProgressbar;
 
@@ -63,5 +82,21 @@ public class HomeFragment extends Fragment {
         int percentage = (int) (((float) availableCredits / (float) totalCredits) * 100);
 
         creditProgressBar.setProgress(percentage);
+    }
+
+    //Sets available credits to current credits of user
+    void getUserCredits(DatabaseReference mDatabase) {
+        mDatabase.child(userId).child("credits").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    String credits = String.valueOf(task.getResult().getValue());
+                    availableCredits = Integer.parseInt(credits);
+                }
+            }
+        });
     }
 }
