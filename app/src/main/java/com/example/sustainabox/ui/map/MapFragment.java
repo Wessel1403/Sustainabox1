81dea05dcf6151f28125981aec8c54ae70306d61
MikeeeZ <m.j.h.v.d.zande@student.tue.nl>
2024-03-13 21:39:02 +0100

    Small UI changes.

--- app/src/main/java/com/example/sustainabox/ui/map/MapFragment.java
+++ app/src/main/java/com/example/sustainabox/ui/map/MapFragment.java
@@ -12,38 +12,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.sustainabox.R;
 import com.example.sustainabox.databinding.FragmentMapBinding;
-import com.google.android.gms.location.FusedLocationProviderClient;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.SearchView;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.os.Bundle;
-import android.widget.FrameLayout;
-import android.widget.Toast;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
 public class MapFragment extends Fragment {
 
     private FragmentMapBinding binding;
@@ -56,8 +26,6 @@ public class MapFragment extends Fragment {
         binding = FragmentMapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textMap;
-        mapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
